e28635c031d7e43cb7addbe36543cc57ef67f33a
TearablePuns <jmemmott13@gmail.com>
2018-07-23 12:25:17 -0600

    After some issues with Mongo, had to create a new project, then installed redux-thunk and axios, built auth actions

---
+++ src/components/auth/signup.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import SignupForm from './signupForm';
+
+class Signup extends Component {
+
+    onSubmit = (fields) => {
+        
+    }
+
+    render() {
+        return (
+            <div className='sign-up'>
+                <SignupForm onSubmit={(event) => this.onSubmit(event)}/>
+            </div>
+        )
+    }
+}
+
+export default Signup;
