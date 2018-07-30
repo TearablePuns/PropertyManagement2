54ccd3372784bb4332696778c73d0cb23a40102f
TearablePuns <jmemmott13@gmail.com>
2018-07-30 09:50:26 -0600

    built newsletterBox and functionality

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import NewsletterBox from './newsletterBox';
 
 class NewsletterGrid extends Component {
     render() {
         return (
             <div className='newsletter-grid'>
-                Semicolon: hide and seek champion since the invention of C
+                {/* add button */}
+                <NewsletterBox date={new Date()}/>
+                {/* archive */}
+                {/* content */}
             </div>
         )
     }
