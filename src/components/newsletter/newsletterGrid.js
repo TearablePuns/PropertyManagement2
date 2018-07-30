4c5949b28d02070c1e3e462dfeda66317e290df1
TearablePuns <jmemmott13@gmail.com>
2018-07-30 09:16:58 -0600

    built the newsletter grid component and grid

---
+++ src/components/newsletter/newsletterGrid.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class NewsletterGrid extends Component {
+    render() {
+        return (
+            <div className='newsletter-grid'>
+                Semicolon: hide and seek champion since the invention of C
+            </div>
+        )
+    }
+}
+export default NewsletterGrid;
