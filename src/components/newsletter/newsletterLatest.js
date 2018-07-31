8057938036d2b315cf3c301440431ef045bda1fe
TearablePuns <jmemmott13@gmail.com>
2018-07-31 09:13:53 -0600

    styled newsletter latest and applied row gaps to the newsletter grid

--- src/components/newsletter/newsletterLatest.js
+++ src/components/newsletter/newsletterLatest.js
@@ -7,7 +7,7 @@ class NewsletterLatest extends Component {
             <h1 className='newsletter-latest__title'>Title goes here</h1>
             <img className='newsletter-latest__image' src='http://via.placeholder.com/960x258' />
             <div className='newsletter-latest__body'>
-                <p>Lorem Ipsum</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas sed sed risus pretium quam vulputate. Tristique magna sit amet purus gravida quis blandit. Tempus egestas sed sed risus pretium. Sed id semper risus in hendrerit gravida rutrum quisque. Iaculis at erat pellentesque adipiscing commodo elit at. Et ultrices neque ornare aenean. Faucibus et molestie ac feugiat sed lectus vestibulum mattis. Id diam vel quam elementum pulvinar etiam non. Leo vel orci porta non pulvinar neque laoreet suspendisse. Amet cursus sit amet dictum sit amet.</p>
             </div>
             </div>
         )
