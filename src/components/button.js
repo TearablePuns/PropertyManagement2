199294eeae48ef10ad848875d8c60f744df1ef91
TearablePuns <jmemmott13@gmail.com>
2018-07-31 10:19:10 -0600

    created and placed add newsletter button on newsletter grid

---
+++ src/components/button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export default function Button({className, callback, text, icon}) {
+    if(icon) {
+        return (
+            <a onClick={callback} className={`${className} button`}>
+            <i className={icon}></i>
+            </a>
+        )
+    }
+}
